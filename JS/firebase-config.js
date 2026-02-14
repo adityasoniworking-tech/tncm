28f7577dac12afb9a2ca3e60216ef8afd313a3e0
adityasoni0326 <cse.220840131132@gmail.com>
2026-02-14 15:51:05 +0530

    Remove all files from GitHub (keeping local copies)

--- JS/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// This file is ignored by Git. Do NOT commit this.
-window.firebaseConfig = {
-    apiKey: "AIzaSyC6Cr8OI7pjTt3t70hrjiSW7kWeZj4jHWc",
-    authDomain: "bakeryapp-c4812.firebaseapp.com",
-    projectId: "bakeryapp-c4812",
-    storageBucket: "bakeryapp-c4812.firebasestorage.app",
-    messagingSenderId: "547764804378",
-    appId: "1:547764804378:web:e4a425b9e13c826afaaaa3"
-};
